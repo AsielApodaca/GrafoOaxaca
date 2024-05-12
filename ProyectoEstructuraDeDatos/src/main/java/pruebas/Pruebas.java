fa940d6a0aff2a1425271244f87058fe1b8f8954
Puert4 <jullian.puerta248239@potros.itson.edu.mx>
2024-05-12 01:14:04 -0700

    Agregue una matrizde adyacencia
    Imprime una matriz de adyacencia en consola

--- ProyectoEstructuraDeDatos/src/main/java/pruebas/Pruebas.java
+++ ProyectoEstructuraDeDatos/src/main/java/pruebas/Pruebas.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package pruebas;
 
 import grafoOaxaca.presentacion.FrmGrafoOaxaca;
@@ -17,9 +16,45 @@ public class Pruebas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        FrmGrafoOaxaca f = new FrmGrafoOaxaca() ;
-        
-        f.setVisible(true);
+        // FrmGrafoOaxaca f = new FrmGrafoOaxaca() ;
+
+        //    f.setVisible(true);
+        double[][] matrizAdyacencia = {
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 8.7, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 4.6, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 7.2, Double.POSITIVE_INFINITY, 7.9},
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 390, 215, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 26.6, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 44.8, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 28.5, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 20.8, Double.POSITIVE_INFINITY},
+            {8.7, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 13, Double.POSITIVE_INFINITY, 6.8},
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 44.8, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 17, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 370, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+            {Double.POSITIVE_INFINITY, 390, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 286, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 167, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+            {4.6, 215, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 255, 5.9},
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 28.5, Double.POSITIVE_INFINITY, 17, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 296, Double.POSITIVE_INFINITY, 40.8, 249},
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 286, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 140, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+            {Double.POSITIVE_INFINITY, 36.6, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 274, Double.POSITIVE_INFINITY},
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 140, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 146, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 188},
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 370, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 296, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 146, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+            {7.2, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 13, Double.POSITIVE_INFINITY, 167, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 20.8, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 255, 40.8, Double.POSITIVE_INFINITY, 274, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 266},
+            {7.9, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 6.8, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 5.9, 249, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 188, 98.9, Double.POSITIVE_INFINITY, 266, Double.POSITIVE_INFINITY}
+        };
+
+        // Imprimir la matriz de adyacencia en forma de tabla
+        System.out.println("Matriz de Adyacencia:");
+        System.out.print("    ");
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            System.out.printf("%-7d", i + 1);
+        }
+        System.out.println();
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            System.out.printf("%-3d|", i + 1);
+            for (int j = 0; j < matrizAdyacencia[i].length; j++) {
+                if (matrizAdyacencia[i][j] == Double.POSITIVE_INFINITY) {
+                    System.out.printf("%-7s", "No");
+                } else {
+                    System.out.printf("%-7.1f", matrizAdyacencia[i][j]);
+                }
+            }
+            System.out.println();
+        }
     }
 
 }
