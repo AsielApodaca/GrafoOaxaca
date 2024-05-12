a9a09e6886ea4ffa38a2516ba68c66e9bc13ea9c
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-05-12 09:43:50 -0700

    mas formato pa la matriz

--- ProyectoEstructuraDeDatos/src/main/java/pruebas/Pruebas.java
+++ ProyectoEstructuraDeDatos/src/main/java/pruebas/Pruebas.java
@@ -19,22 +19,23 @@ public class Pruebas {
         // FrmGrafoOaxaca f = new FrmGrafoOaxaca() ;
 
         //    f.setVisible(true);
+        double inf = Double.POSITIVE_INFINITY;
         double[][] matrizAdyacencia = {
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 8.7, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 4.6, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 7.2, Double.POSITIVE_INFINITY, 7.9},
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 390, 215, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 26.6, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 44.8, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 28.5, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 20.8, Double.POSITIVE_INFINITY},
-            {8.7, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 13, Double.POSITIVE_INFINITY, 6.8},
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 44.8, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 17, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 370, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
-            {Double.POSITIVE_INFINITY, 390, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 286, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 167, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
-            {4.6, 215, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 255, 5.9},
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 28.5, Double.POSITIVE_INFINITY, 17, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 296, Double.POSITIVE_INFINITY, 40.8, 249},
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 286, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 140, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
-            {Double.POSITIVE_INFINITY, 36.6, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 274, Double.POSITIVE_INFINITY},
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 140, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 146, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 188},
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 370, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 296, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 146, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
-            {7.2, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 13, Double.POSITIVE_INFINITY, 167, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
-            {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 20.8, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 255, 40.8, Double.POSITIVE_INFINITY, 274, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 266},
-            {7.9, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 6.8, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 5.9, 249, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 188, 98.9, Double.POSITIVE_INFINITY, 266, Double.POSITIVE_INFINITY}
+            {inf, inf, inf, 8.7, inf, inf, 4.6, inf, inf, inf, inf, inf, 7.2, inf, 7.9},
+            {inf, inf, inf, inf, inf, 390, 215, inf, inf, 26.6, inf, inf, inf, inf, inf},
+            {inf, inf, inf, inf, 44.8, inf, inf, 28.5, inf, inf, inf, inf, inf, 20.8, inf},
+            {8.7, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, 13, inf, 6.8},
+            {inf, inf, 44.8, inf, inf, inf, inf, 17, inf, inf, inf, 370, inf, inf, inf},
+            {inf, 390, inf, inf, inf, inf, inf, inf, 286, inf, inf, inf, 167, inf, inf},
+            {4.6, 215, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, 255, 5.9},
+            {inf, inf, 28.5, inf, 17, inf, inf, inf, inf, inf, inf, 296, inf, 40.8, 249},
+            {inf, inf, inf, inf, inf, 286, inf, inf, inf, inf, 140, inf, inf, inf, inf},
+            {inf, 36.6, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, 274, inf},
+            {inf, inf, inf, inf, inf, inf, inf, inf, 140, inf, inf, 146, inf, inf, 188},
+            {inf, inf, inf, inf, 370, inf, inf, 296, inf, inf, 146, inf, inf, inf, inf},
+            {7.2, inf, inf, 13, inf, 167, inf, inf, inf, inf, inf, inf, inf, inf, inf},
+            {inf, inf, 20.8, inf, inf, inf, 255, 40.8, inf, 274, inf, inf, inf, inf, 266},
+            {7.9, inf, inf, 6.8, inf, inf, 5.9, 249, inf, inf, 188, 98.9, inf, 266, inf}
         };
 
         // Imprimir la matriz de adyacencia en forma de tabla
